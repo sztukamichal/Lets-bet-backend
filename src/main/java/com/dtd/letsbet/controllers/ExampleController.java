7eb0b564cb4a5561ee00f9f52098e7ca156a75fa
Michał <sztuuka.michal@gmail.com>
2017-05-18 01:07:30 +0200

    Rest test

---
+++ src/main/java/com/dtd/letsbet/controllers/ExampleController.java
@@ -0,0 +1,19 @@
+package com.dtd.letsbet.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mac on 18.05.2017.
+ */
+@RestController
+@RequestMapping("/example")
+public class ExampleController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getGreeting() {
+        return "Hello";
+    }
+
+}
