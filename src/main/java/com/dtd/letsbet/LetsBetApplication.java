af631763f23a97a40a5d7fc1fb7df6178453d9c6
Michał <sztuuka.michal@gmail.com>
2017-05-18 00:23:55 +0200

    first commit

---
+++ src/main/java/com/dtd/letsbet/LetsBetApplication.java
@@ -0,0 +1,12 @@
+package com.dtd.letsbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsBetApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dtd/letsbet/LetsBetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dtd.letsbet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LetsBetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
