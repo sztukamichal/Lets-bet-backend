bbb1e6907718d1fc8db7a2e5f4bf5815443b0536
Maciej <maciej.owczarczak@gmail.com>
2017-06-11 00:08:57 +0200

    Reading ready - some refactoring left and saving to database - waiting for Azia :D

--- src/main/java/com/dtd/letsbet/externDataProvider/DataProvider.java
+++ src/main/java/com/dtd/letsbet/externDataProvider/DataProvider.java
@@ -1,73 +1,192 @@
 package com.dtd.letsbet.externDataProvider;
 
-import net.minidev.json.JSONObject;
-import net.minidev.json.parser.JSONParser;
-import net.minidev.json.parser.ParseException;
-
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
-import java.net.URLEncoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
+
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;;
 
 /**
  * Created by Maciej on 04.06.2017.
  */
 public class DataProvider {
-    public DataProvider(){}
+    private List<String> teamList;
+    private List<String> leagueList;
+
+    private static String competition = "http://api.football-data.org/v1/competitions/";
+
+
+    public DataProvider(){
+        teamList = new  ArrayList();
+        leagueList = new  ArrayList();
+    }
 
     enum sourceType{
-        //write model with path from which we take data
-        Competition("competitions/");
+        Competition(false, competition),
+        Teams(true),
+        Leagues(true);
 
-        private final String text;
+        private final boolean isList;
+        private final String uri;
 
-        private sourceType(final String text){
-            this.text = text;
+        private sourceType(final boolean isList, final String uri){
+            this.isList = isList;
+            this.uri = uri;
+        }
+
+        private sourceType(final boolean isList){
+            this.isList = isList;
+            this.uri = "";
+        }
+
+        public boolean isList(){
+            return isList;
         }
 
         @Override
         public String toString(){
-            return text;
+            return uri;
         }
     }
 
-    private void getData(){
-        //partially so you can see how it looks like, rest of it and transaltions too model is a long work - i will push it as soon as ready
 
+    public void getData(){
         for(sourceType source : sourceType.values()) {
-            convertToModel(source);
+            if(source.isList){
+                switch(source){
+                    case Teams:
+                        readFromList(source, teamList);
+                        break;
+                    case Leagues:
+                        readFromList(source, leagueList);
+                        break;
+                }
+            }
+            else{
+                Object json = getJSONObject(source.toString());
+                undressJSON(source, json);
+            }
         }
     }
 
-    private void convertToModel(sourceType source){
-        JSONObject json = getJSONObject(source.toString());
+    private void readFromList(sourceType source, List<String> list) {
+        for(String uri : list){
+            Object json = getJSONObject(uri);
+            undressJSON(source, json);
+        }
+    }
+
+    private void undressJSON(sourceType source, Object json) {
+        if(json instanceof JSONObject ){
+            saveToDatabase(source, (JSONObject)json);
+        }
+        else if(json instanceof JSONArray){
+            for(Object probableJSONObject : ((JSONArray) json).toArray()){
+                if(probableJSONObject instanceof JSONObject ){
+                    saveToDatabase(source, (JSONObject)probableJSONObject);
+                }
+                else if(probableJSONObject instanceof JSONArray){
+                    undressJSON(source, probableJSONObject);
+                }
+            }
+        }
+    }
+
+    private void saveToDatabase(sourceType source, JSONObject jsonObject){
         switch(source){
             case Competition:
-                //to do - convert to model
+                saveCompetition(jsonObject);
+                break;
+            case Teams:
+                saveTeams(jsonObject);
+                break;
+            case Leagues:
+                saveLeaguesAndStandings(jsonObject);
                 break;
         }
     }
 
+    private void saveCompetition(JSONObject jsonObject) {
+        fillListsFromCompetition(getJsonObjectElement(jsonObject, "_links"));
+        translateCompetition(jsonObject);
+    }
 
-    private JSONObject getJSONObject(String additionalUri){
-        String uri = "http://api.football-data.org/v1/" +  additionalUri;
+    private void saveTeams(JSONObject jsonObject) {
+        JSONArray teams = (JSONArray) getJsonObjectElement(jsonObject, "teams");
+        for(Object teamJSONObject : ((JSONArray) teams).toArray()){
+            translateTeams(jsonObject);
+        }
+    }
+
+    private void saveLeaguesAndStandings(JSONObject jsonObject) {
+        translateLeagues(jsonObject);
+        translateStandings(jsonObject);
+    }
+
+    private void fillListsFromCompetition(Object jsonLinks) {
+        Object teamObject = getJsonObjectElement(((JSONObject)jsonLinks), "teams");
+        String uri = (String) getJsonObjectElement(((JSONObject)teamObject), "href");
+        if(!teamList.contains(uri)){
+            teamList.add(uri);
+        }
+    }
+
+    private void translateCompetition(JSONObject jsonObject) {
+        int externalId = (int) getJsonObjectElement(jsonObject, "id");
+        String caption= (String) getJsonObjectElement(jsonObject, "caption");
+        String leagueCode= (String) getJsonObjectElement(jsonObject, "league");
+        String year = (String) getJsonObjectElement(jsonObject, "year");
+        int numberOfTeams= (int) getJsonObjectElement(jsonObject, "numberOfTeams");
+        int numberOfGames= (int) getJsonObjectElement(jsonObject, "numberOfGames");
+        Date lastUpdated= translateJsonDate((String) getJsonObjectElement(jsonObject, "lastUpdated"));
+        int currentMatchday= (int) getJsonObjectElement(jsonObject, "currentMatchday");
+        int numberOfMatchdays= (int) getJsonObjectElement(jsonObject, "numberOfMatchdays");
+    }
+
+    private void translateTeams(JSONObject jsonObject){
+        String name = (String) getJsonObjectElement(jsonObject, "name");
+        String code = (String) getJsonObjectElement(jsonObject, "code");
+        String shortName = (String) getJsonObjectElement(jsonObject, "shortName");
+        String squadMarketValue = (String) getJsonObjectElement(jsonObject, "squadMarketValue");
+        String crestUrl = (String) getJsonObjectElement(jsonObject, "crestUrl");
+    }
+
+    private void translateLeagues(JSONObject jsonObject){
+        String leagueCaption = (String) getJsonObjectElement(jsonObject, "leagueCaption");
+        String matchday = (String) getJsonObjectElement(jsonObject, "matchday");
+    }
+
+    private void translateStandings(JSONObject jsonObject) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    private Object getJSONObject(String uri){
         String json = readJsonFromSite(uri);
 
+        @SuppressWarnings("deprecation")
         JSONParser parser = new JSONParser();
         try {
-            JSONObject obj = (JSONObject) parser.parse(json);
+            Object obj = parser.parse(json);
             return obj;
         }
-        catch(ParseException e){
+        catch(net.minidev.json.parser.ParseException e){
             return null;
         }
     }
 
     private String readJsonFromSite(String uri){
         try{
-            String encodedUri = URLEncoder.encode(uri, "UTF-8");
-            URL url = new URL(encodedUri);
+            URL url = new URL(uri);
 
             Scanner scan = new Scanner(url.openStream());
             String json = "";
@@ -84,4 +203,16 @@ public class DataProvider {
         }
     }
 
+    private Date translateJsonDate(String jsonDate){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return sdf.parse(jsonDate);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+    private Object getJsonObjectElement(JSONObject obj, String name){
+        return obj.get(name);
+    }
 }
